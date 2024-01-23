ae39cc85f70b07647fbd46777a4f72d88945d97d
AnasAlbukaai <https://github.com/AnasAlbukaai/alkarama-sport-club>
2024-01-23 18:40:07 +0200

    firstcomment

--- lib/ui/shared/custom _widgets/custom_container.dart
+++ lib/ui/shared/custom _widgets/custom_container.dart
@@ -18,62 +18,56 @@ class MyContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Center(
-          child: Container(
-            width: screenWidth(1.8),
-            height: screenWidth(1.3),
-            decoration: BoxDecoration(
-                color: AppColors.whiteColor,
-                border: Border.all(color: AppColors.blueColor, width: 2),
-                borderRadius: BorderRadius.circular(10)),
-            child: Column(
-              children: [
-                Padding(
-                  padding: EdgeInsetsDirectional.only(
-                    end: screenWidth(40),
-                    top: screenWidth(40),
-                    start: screenWidth(40),
+    return Container(
+      width: screenWidth(1.8),
+      height: screenWidth(1.3),
+      decoration: BoxDecoration(
+          color: AppColors.whiteColor,
+          border: Border.all(color: AppColors.blueColor, width: 2),
+          borderRadius: BorderRadius.circular(10)),
+      child: Column(
+        children: [
+          Padding(
+            padding: EdgeInsetsDirectional.only(
+              end: screenWidth(40),
+              top: screenWidth(40),
+              start: screenWidth(40),
+            ),
+            child: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image.asset(
+                    'assets/images/pngs/$iconName',
                   ),
-                  child: Container(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Image.asset(
-                          'assets/images/pngs/$iconName',
-                        ),
-                        CustomText(
-                          text: title,
-                          styleType: TextStyleType.SUBTITLE,
-                          textColor: AppColors.whiteColor,
-                          fontWeight: FontWeight.w700,
-                        )
-                      ],
-                    ),
-                    decoration: BoxDecoration(
-                        color: AppColors.blueColor,
-                        borderRadius: BorderRadius.circular(10)),
-                    width: screenWidth(2),
-                    height: screenWidth(4),
-                  ),
-                ),
-                SizedBox(
-                  height: screenWidth(50),
-                ),
-                Padding(
-                  padding: EdgeInsetsDirectional.only(start: screenWidth(50)),
-                  child: CustomText(
-                    text: text,
-                    styleType: TextStyleType.BODY,
-                    fontWeight: FontWeight.w500,
-                    textColor: AppColors.blackColor,
-                  ),
-                )
-              ],
+                  CustomText(
+                    text: title,
+                    styleType: TextStyleType.SUBTITLE,
+                    textColor: AppColors.whiteColor,
+                    fontWeight: FontWeight.w700,
+                  )
+                ],
+              ),
+              decoration: BoxDecoration(
+                  color: AppColors.blueColor,
+                  borderRadius: BorderRadius.circular(10)),
+              width: screenWidth(2),
+              height: screenWidth(4),
             ),
           ),
-        ),
+          SizedBox(
+            height: screenWidth(50),
+          ),
+          Padding(
+            padding: EdgeInsetsDirectional.only(start: screenWidth(50)),
+            child: CustomText(
+              text: text,
+              styleType: TextStyleType.BODY,
+              fontWeight: FontWeight.w500,
+              textColor: AppColors.blackColor,
+            ),
+          )
+        ],
       ),
     );
   }
--- lib/ui/shared/shared_widgets/custom_tob_container.dart
+++ lib/ui/shared/shared_widgets/custom_tob_container.dart
@@ -17,58 +17,54 @@ class TobContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Container(
-          width: screenWidth(1),
-          height: screenWidth(3.6),
-          decoration: BoxDecoration(
-              color: AppColors.blueColor,
-              borderRadius: BorderRadiusDirectional.only(
-                  bottomEnd: Radius.circular(25),
-                  bottomStart: Radius.circular(25))),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Container(
+      width: screenWidth(1),
+      height: screenWidth(3.6),
+      decoration: BoxDecoration(
+          color: AppColors.blueColor,
+          borderRadius: BorderRadiusDirectional.only(
+              bottomEnd: Radius.circular(25),
+              bottomStart: Radius.circular(25))),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+            padding: EdgeInsetsDirectional.only(start: screenWidth(25)),
+            child: Row(
+              children: [
+                isimageball
+                    ? Image.asset(
+                        'assets/images/pngs/ic_ball.png',
+                        width: screenWidth(8),
+                      )
+                    : Container(
+                        width: screenWidth(7),
+                        height: screenWidth(7),
+                        color: AppColors.blueColor,
+                      ),
+                Icon(
+                  iconArrowForward,
+                  size: screenWidth(15),
+                  color: AppColors.whiteColor,
+                )
+              ],
+            ),
+          ),
+          Row(
             children: [
-              Padding(
-                padding: EdgeInsetsDirectional.only(start: screenWidth(25)),
-                child: Row(
-                  children: [
-                    isimageball
-                        ? Image.asset(
-                            'assets/images/pngs/ic_ball.png',
-                            width: screenWidth(8),
-                          )
-                        : Container(
-                            width: screenWidth(7),
-                            height: screenWidth(7),
-                            color: AppColors.blueColor,
-                          ),
-                    Icon(
-                      iconArrowForward,
-                      size: screenWidth(15),
-                      color: AppColors.whiteColor,
-                    )
-                  ],
-                ),
+              CustomText(
+                text: text,
+                styleType: TextStyleType.SUBTITLE,
+                textColor: AppColors.whiteColor,
+                fontWeight: FontWeight.w400,
               ),
-              Row(
-                children: [
-                  CustomText(
-                    text: text,
-                    styleType: TextStyleType.SUBTITLE,
-                    textColor: AppColors.whiteColor,
-                    fontWeight: FontWeight.w400,
-                  ),
-                  Image.asset(
-                    'assets/images/pngs/ic_alkarama.png',
-                    width: screenWidth(7),
-                  )
-                ],
-              ),
+              Image.asset(
+                'assets/images/pngs/ic_alkarama.png',
+                width: screenWidth(7),
+              )
             ],
           ),
-        ),
+        ],
       ),
     );
   }
