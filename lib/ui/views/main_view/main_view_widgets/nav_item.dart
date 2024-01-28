6d64f054a46f4b9347ec8149fe5fdfb80be291bf
AnasAlbukaai <eng.anasalbukaai@gmail.com>
2024-01-28 19:11:03 +0300

    navigation bar

---
+++ lib/ui/views/main_view/main_view_widgets/nav_item.dart
@@ -0,0 +1,52 @@
+import 'package:alkarama_sport_club/core/enums/text_style_type.dart';
+import 'package:alkarama_sport_club/ui/shared/colors.dart';
+import 'package:alkarama_sport_club/ui/shared/custom%20_widgets/custom_text.dart';
+import 'package:alkarama_sport_club/ui/shared/utlis.dart';
+import 'package:flutter/material.dart';
+
+class NavItem extends StatelessWidget {
+  final String icon;
+  final bool isSelected;
+  final Function onTap;
+  final String title;
+
+  NavItem({
+    super.key,
+    required this.icon,
+    required this.isSelected,
+    required this.onTap,
+    required this.title,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        onTap();
+      },
+      child: Column(
+        children: [
+          Spacer(),
+          // Container(
+          //   decoration: BoxDecoration(
+          //       border: isSelected
+          //           ? Border(
+          //               bottom:
+          //                   BorderSide(color: AppColors.blueColor, width: 4))
+          //           : null),
+          // ),
+          Image.asset(
+            icon,
+            width: screenWidth(10),
+            // color: AppColors.whiteColor,
+          ),
+          CustomText(
+            text: title,
+            styleType: TextStyleType.SUBTITLE,
+            textColor: isSelected ? AppColors.whiteColor : AppColors.goldColor,
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/ui/views/main_view/sccore_view/sccore_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SccoreView extends StatelessWidget {
+  const SccoreView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(),
+    );
+  }
+}
