a03102f2082769d59557013a211343c90440d327
RamaDabdoub <rama002211@gmail.com>
2024-01-30 17:12:17 +0300

    add img

--- lib/ui/shared/shared_widgets/custom_tob_container.dart
+++ lib/ui/shared/shared_widgets/custom_tob_container.dart
@@ -1,6 +1,6 @@
 import 'package:alkarama_sport_club/core/enums/text_style_type.dart';
 import 'package:alkarama_sport_club/ui/shared/colors.dart';
-import 'package:alkarama_sport_club/ui/shared/shared_widgets/custom_text.dart';
+import 'package:alkarama_sport_club/ui/shared/custom%20_widgets/custom_text.dart';
 import 'package:alkarama_sport_club/ui/shared/utlis.dart';
 import 'package:flutter/material.dart';
 
