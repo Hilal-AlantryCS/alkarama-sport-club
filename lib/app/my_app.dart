f7fcf079c8fe4b300ff04b2a24a3d08cacb6c9ef
AnasAlbukaai <eng.anasalbukaai@gmail.com>
2024-01-30 18:32:00 +0300

    save

--- lib/app/my_app.dart
+++ lib/app/my_app.dart
@@ -2,6 +2,7 @@ import 'package:alkarama_sport_club/core/enums/connectivity_status.dart';
 import 'package:alkarama_sport_club/ui/shared/shared_widgets/custom_tob_container.dart';
 import 'package:alkarama_sport_club/ui/shared/utlis.dart';
 import 'package:alkarama_sport_club/ui/views/player_and_admins_view/player_and_admins_view.dart';
+import 'package:alkarama_sport_club/ui/views/player_view/player_view.dart';
 import 'package:bot_toast/bot_toast.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -32,14 +33,6 @@ class MyApp extends StatelessWidget {
               colorScheme: ColorScheme.fromSeed(
                   seedColor: Color.fromRGBO(1, 171, 237, 100)),
             ),
-            home: Column(
-              children: [
-                TobContainer(
-                  isimageball: true,
-                  iconArrowForward: Icons.arrow_forward_ios_rounded,
-                  text: 'النادي',
-                ),
-              ],
-            )));
+            home: PlayerView()));
   }
 }
